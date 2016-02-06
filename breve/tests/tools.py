11bed13ae70e629f898d02c2d3b818a566500d05
Mathias Seidler <seishin@gmail.com>
2016-02-06 22:34:27 +0100

    Convert tools.py to pytest style.

--- breve/tests/tools.py
+++ breve/tests/tools.py
@@ -1,73 +1,41 @@
 # -*- coding: utf-8 -*-
-import os
-import unittest
-
 from breve.flatten import flatten
 from breve.plugin.helpers import render_decorator
 from breve.tags.entities import entities
 from breve.tags.html import tags
-from breve.tests.lib import diff, expected_output, template_root, test_root
-
-try:
-    from breve.tools.soup2breve import convert_file, meta_handler
-    souptests = True
-except ImportError:
-    souptests = False
+from breve.tests.lib import expected_output, template_root, test_root
 
 
-class Soup2BreveTestCase(unittest.TestCase):
-
-    # disabled because maybe bs4 does some things differently but i don't use it anyway
-    def disabled_test_soup2breve(self):
-        """ round-trip some html """
-
-        breve_source = ''.join(
-            convert_file(
-                os.path.join(test_root(), 'html/index.html'),
-                dict(meta=meta_handler)
-            )
+# disabled because maybe bs4 does some things differently but i don't use it anyway
+def disabled_test_soup2breve():
+    """ round-trip some html """
+    import os
+    try:
+        from breve.tools.soup2breve import convert_file, meta_handler
+    except ImportError:
+        return
+    breve_source = ''.join(
+        convert_file(
+            os.path.join(test_root(), 'html/index.html'),
+            dict(meta=meta_handler)
         )
-        code_object = compile(breve_source, 'soup2breve', 'eval')
+    )
+    code_object = compile(breve_source, 'soup2breve', 'eval')
 
-        _globals = dict(E=entities)
-        _globals.update(tags)
+    _globals = dict(E=entities)
+    _globals.update(tags)
 
-        actual = flatten(eval(code_object, _globals))
-        expected = open(os.path.join(test_root(), 'html/index.html')).read()
-
-        try:
-            # we can't actually round trip because attributes never come out in
-            # the same order twice =(
-            self.assertEqual(len(actual), len(expected))
-        except AssertionError:
-            diff(actual, expected)
-            raise
+    actual = flatten(eval(code_object, _globals))
+    expected = open(os.path.join(test_root(), 'html/index.html')).read()
+    assert actual == expected
 
 
+def test_render_decorator():
+    """test helpers.render_decorator"""
+    @render_decorator('index', root=template_root(), namespace='v')
+    def render_test():
+        return dict(title='test decorator', message='hello, world')
 
-class PluginHelpersTestCase(unittest.TestCase):
-
-    def test_render_decorator(self):
-        """test helpers.render_decorator"""
-
-        @render_decorator('index', root=template_root(), namespace='v')
-        def render_test():
-            return dict(title='test decorator', message='hello, world')
-
-        actual = render_test()
-        expected = expected_output()
-
-        self.assertEqual(actual, expected)
-
-
-def suite():
-    suite = unittest.TestSuite()
-
-    if souptests:
-        suite.addTest(unittest.makeSuite(Soup2BreveTestCase, 'test'))
-    suite.addTest(unittest.makeSuite(PluginHelpersTestCase, 'test'))
-
-    return suite
-
-if __name__ == '__main__':
-    unittest.main(defaultTest='suite')
+    actual = render_test()
+    expected = expected_output()
+    assert actual == expected
